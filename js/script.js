685b2802a73e7ad6cd12fede6f6305c15559e6b2
maggie <maggie.papadimas@hotmail.com>
2019-02-07 00:31:56 -0500

    added comments

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
 const cards = document.querySelectorAll('.deck');
 
+//if card is flipped
 let cardFlipped = false;
+//for when a card is chosen
 let cardsChosen = false;
+//for matching the pairs
 let card01, card02;
 
+//setting random poistion. and adds listener for clicks
 for(let i = 0; i < cards.length; i+=1 ){
     cards[i].style.order = Math.ceil(Math.random() * 12);
-    cards[i].addEventListener('click', game);
+    cards[i].addEventListener('click', check);
 }
 
-function newGame() {
+//reflips cards
+function reset() {
   cardFlipped = false;
   cardsChosen = false;
   card01 = null;
   card02 = null;
 }
 
+//flips the cards
 function flipCards() {
-  card01.removeEventListener('click', game);
-  card02.removeEventListener('click', game);
+  card01.removeEventListener('click', check);
+  card02.removeEventListener('click', check);
 
-  newGame();
+  reset();
 }
 
-function game() {
+//checks for match
+function check() {
   if (cardsChosen || this === card01) return;
 
   this.classList.add('flip');
@@ -41,18 +48,15 @@ function game() {
     flipCards();
   }
   else {
-    unflipCards();
+    setTimeout(unflipTimer, 1000);
   }
 }
 
-function unflipCards() {
-  setTimeout(unflipTimer, 1000);
-}
-
+//flips unmatched cards
 function unflipTimer() {
 
     card01.classList.remove('flip');
     card02.classList.remove('flip');
 
-    newGame();
+    reset();
 }

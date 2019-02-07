f7940bc0e37f224d59717665319749a0c22ac582
maggie <maggie.papadimas@hotmail.com>
2019-02-07 00:16:03 -0500

    finally works.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,58 @@
 const cards = document.querySelectorAll('.deck');
-cards.forEach(card => card.addEventListener('click', flip));
 
 let cardFlipped = false;
+let cardsChosen = false;
 let card01, card02;
 
-function flip (){
-  this.classList.toggle('flipped');
+for(let i = 0; i < cards.length; i+=1 ){
+    cards[i].style.order = Math.ceil(Math.random() * 12);
+    cards[i].addEventListener('click', game);
+}
 
-  if (cardFlipped) {
+function newGame() {
+  cardFlipped = false;
+  cardsChosen = false;
+  card01 = null;
+  card02 = null;
+}
+
+function flipCards() {
+  card01.removeEventListener('click', game);
+  card02.removeEventListener('click', game);
+
+  newGame();
+}
+
+function game() {
+  if (cardsChosen || this === card01) return;
+
+  this.classList.add('flip');
+
+  if (!cardFlipped) {
     cardFlipped = true;
     card01 = this;
+    return;
   }
+
+  card02 = this;
+  cardsChosen = true;
+
+  if(card01.dataset.name === card02.dataset.name){
+    flipCards();
+  }
+  else {
+    unflipCards();
+  }
+}
+
+function unflipCards() {
+  setTimeout(unflipTimer, 1000);
+}
+
+function unflipTimer() {
+
+    card01.classList.remove('flip');
+    card02.classList.remove('flip');
+
+    newGame();
 }
